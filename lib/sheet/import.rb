c22d98dc801c3616d095242d5bea8888969b4c3a
Xero <vhector.1337@gmail.com>
2023-06-14 20:23:37 -0400

    import from csv sheet implementation

--- lib/sheet/import.rb
+++ lib/sheet/import.rb
@@ -1,5 +1,31 @@
+require 'csv'
+
 module Sheet
   class Import
+    # @return [String]
+    CACTI = File.join(Dir.home, 'Downloads', "cacti.csv")
+
+    # @return [String]
+    TROPICAL = File.join(Dir.home, 'Downloads', "tropical.csv")
+
+    # @return [String]
+    SUCCULENTS = File.join(Dir.home, 'Downloads', "succulents.csv")
+
+
+    # @return [void]
+    def execute
+      [CACTI, TROPICAL, SUCCULENTS].each do |sheet|
+        rows = CSV.read(sheet, headers: true)
+        rows.each do |row|
+          genus = Genus.find_or_create_by!(name: row["Genus"])
+          family = Family.find_or_create_by!(name: row["Family"])
+          Plant.find_or_create_by!(name: row["Name"], genus: genus, family: family)
+        end
+
+        Rails.logger.info("imported #{rows.length} plant(s) from #{sheet}")
+      end
+    end
+
     class << self
       # @return [void]
       def execute
