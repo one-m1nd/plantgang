fb3d5e9ad2fc83223b0d0fa3b09079b41494c489
Xero <vhector.1337@gmail.com>
2023-06-24 19:12:09 -0400

    replace plant table with react data grid

--- app/javascript/components/PlantGrid.js
+++ app/javascript/components/PlantGrid.js
@@ -5,14 +5,16 @@ import DataGrid from 'react-data-grid';
 
 const columns = [
   { key: 'id', name: 'ID' },
-  { key: 'name', name: 'Plant' }
+  { key: 'name', name: 'Plant' },
+  { key: 'family', name: 'Family' },
+  { key: 'genus', name: 'Genus' }
 ];
 
 
 class PlantGrid extends React.Component {
   render () {
     var plants = JSON.parse(this.props.plants);
-    var rows = plants.map(x => ({id: x.id, name: x.name}));
+    var rows = plants.map(x => ({ id: x.id, name: x.name, family: x.family, genus: x.genus }));
 
     return (
         <DataGrid columns={columns} rows={rows} />
