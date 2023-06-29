19494d33045733d6210051eebe50831bc9f8f75c
Xero <vhector.1337@gmail.com>
2023-06-28 20:41:15 -0400

    add select column

--- app/javascript/components/PlantGrid.js
+++ app/javascript/components/PlantGrid.js
@@ -1,27 +1,34 @@
 import React from "react"
 import PropTypes from "prop-types"
 
-import DataGrid from 'react-data-grid';
+import DataGrid, { SelectColumn } from 'react-data-grid';
 
 const columns = [
+  SelectColumn,
   { key: 'id', name: 'ID' },
   { key: 'name', name: 'Plant' },
   { key: 'family', name: 'Family' },
   { key: 'genus', name: 'Genus' }
 ];
 
+let rows = [];
+
 function rowGetter(row) {
   return row.id;
+}
+
+function PlantGridGenerator() {
+  const [selectedRows, setSelectedRows] = React.useState(() => new Set());
+
+  return (<DataGrid columns={columns} rows={rows} rowKeyGetter={rowGetter} onSelectedRowsChange={setSelectedRows} selectedRows={selectedRows}/>);
 }
 
 class PlantGrid extends React.Component {
   render () {
     let plants = JSON.parse(this.props.plants);
-    let rows = plants.map(x => ({ id: x.id, name: x.name, family: x.family, genus: x.genus }));
+    rows = plants.map(x => ({ id: x.id, name: x.name, family: x.family, genus: x.genus }));
 
-    return (
-        <DataGrid columns={columns} rows={rows} rowKeyGetter={rowGetter}/>
-    );
+    return (<PlantGridGenerator/>);
   }
 }
 
