5cd47e41fd067c9c6cd4bd3492511ac3ca3189e2
Xero <vhector.1337@gmail.com>
2024-02-26 11:55:05 -0500

    add some caching so that we don't hammer the api

--- app/controllers/sunlight_hours_controller.rb
+++ app/controllers/sunlight_hours_controller.rb
@@ -1,5 +1,7 @@
 class SunlightHoursController < ApplicationController
   def index
-    @sunlight_hours = ShowSunlightHours.show
+    @sunlight_hours = Rails.cache.fetch(:sunlight_hours_value, expires_in: 15.minutes) do
+      ShowSunlightHours.show
+    end
   end
 end
