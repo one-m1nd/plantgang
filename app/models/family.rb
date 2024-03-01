3773970b677ca5bf304bd8ec2b839f41b12cf888
Xero <vhector.1337@gmail.com>
2024-03-01 09:42:15 -0500

    trefle data accessor for family model

--- app/models/family.rb
+++ app/models/family.rb
@@ -1,4 +1,15 @@
 class Family < ApplicationRecord
   has_many :plants
   has_many :genera, through: :plants
+
+  def trefle_data
+    Rails.cache.fetch("#{name}-trefle", expires_in: 15.minutes) do
+      response = Plants.find_family(name.downcase)
+      if response.status.success?
+        response.parse['data']
+      else
+        {}
+      end
+    end
+  end
 end
