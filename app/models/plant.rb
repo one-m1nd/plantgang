8881e2df3cb6ec8d9dd44b5b908c9182a33d4ae5
Xero <vhector.1337@gmail.com>
2024-03-04 10:18:46 -0500

    why doesn't this work flat out the box ... hmmge

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -3,6 +3,10 @@ class Plant < ApplicationRecord
   belongs_to :genus
   has_one :status
 
+  def status
+    Status.find(status_id)
+  end
+
   # @return [Hash]
   def trefle_data
     Rails.cache.fetch("#{name}-trefle", expires_in: 15.minutes) do
