1e7893695ae868f095a1e6f7db460a09080ba5eb
Xero <vhector.1337@gmail.com>
2024-03-06 14:35:21 -0500

    allow show to be via name for genera and families as well

--- app/controllers/families_controller.rb
+++ app/controllers/families_controller.rb
@@ -61,7 +61,8 @@ class FamiliesController < ApplicationController
   private
   # Use callbacks to share common setup or constraints between actions.
   def set_family
-    @family = Family.find(params[:id])
+    @family = Family.find_by(name: params[:id])
+    @family ||= Family.find(params[:id])
   end
 
   # Only allow a list of trusted parameters through.
--- app/controllers/genera_controller.rb
+++ app/controllers/genera_controller.rb
@@ -61,7 +61,8 @@ class GeneraController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_genus
-      @genus = Genus.find(params[:id])
+      @genus = Genus.find_by(name: params[:id])
+      @genus ||= Genus.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
